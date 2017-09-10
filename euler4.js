787711dc188a53a6a7399a651001a70894958ebf
nealszentmiklosy <nszent@gmail.com>
2017-09-10 18:09:18 -0400

    incorporated palindrome code into main problem

--- euler4.js
+++ euler4.js
@@ -1,7 +1,9 @@
-let input = 9009;
-function ispalin(number){
-  let n = number.toString();
-  for (i=0;i<string.length;i++)
-	  console.log(string.charAt(i))
+let test = 90909;
+function isPalindrome(number){
+    let n = number.toString();
+    for (i=0;i<Math.floor(n.length/2);i++){
+	    if(n.charAt(i) !== n.charAt(n.length-1-i)){return false}
+    }
+    return true
 }
-ispalin(input)
+console.log(isPalindrome(test))
