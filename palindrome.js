a5b746fab82b11e8e3e87bcbe22d1aef8a41e907
nealszentmiklosy <nszent@gmail.com>
2017-09-10 18:03:22 -0400

    fixed the palindrome code

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,8 @@
-let number = 90909;
+let isPalindrome = true;
+let number = 909099999;
 let n = number.toString();
 for (i=0;i<Math.floor(n.length/2);i++){
-	if(n.charAt(i) == n.charAt(n.length-1-i)){console.log("yes")}
-		else{console.log("no")}
+	if(n.charAt(i) !== n.charAt(n.length-1-i)){isPalindrome = false}
 }
+// isPalindrome = true
+console.log(isPalindrome)
